0ea17c671901cee3a9f97b719a6bcf45f04e0cc0
vivere-dally <stefanbuciujr@gmail.com>
2026-06-11 15:56:03 +0300

    Add solid to eslint

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,5 +1,6 @@
 import js from "@eslint/js";
 import globals from "globals";
+import solid from "eslint-plugin-solid/configs/typescript";
 import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
 
@@ -9,6 +10,22 @@ export default defineConfig([
         plugins: { js },
         extends: ["js/recommended"],
         languageOptions: { globals: globals.browser },
+        rules: {
+            "@typescript-eslint/no-unused-vars": [
+                "error",
+                {
+                    argsIgnorePattern: "^_",
+                    destructuredArrayIgnorePattern: "^_",
+                    caughtErrorsIgnorePattern: "^_",
+                    varsIgnorePattern: "^_",
+                },
+            ],
+        },
     },
     tseslint.configs.recommended,
+    // @ts-expect-error -- eslint-plugin-solid types lag behind eslint v10's defineConfig
+    {
+        files: ["**/*.{ts,tsx}"],
+        ...solid,
+    },
 ]);
